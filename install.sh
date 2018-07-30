d6b2a7f546862bc625f58790e44b1ab1e0f50cb0
raphael <raphael@zetapush.com>
2018-07-30 16:08:00 +0200

    rename scpt

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ SCRIPTS_PATH="$HOME/Library/Scripts/Folder Action Scripts"
 
 mkdir -p "$SCRIPTS_PATH"
 curl -sSO https://raw.githubusercontent.com/raphael22/svgo-osx-folder-action/master/svgo.applescript
-osacompile -o "$SCRIPTS_PATH/svgo.scpt" svgo.applescript
+osacompile -o "$SCRIPTS_PATH/svgo_raf.scpt" svgo.applescript
 rm svgo.applescript
